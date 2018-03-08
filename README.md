46504fbdb5c97b5c15bbcec67d52cda4ff328df2
kbulis <kirk.d.bulis@gmail.com>
2018-03-08 02:15:22 -0800

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# express-jwt-xsrf
-Express filter for jwt parsing with anti-xsrf synchronizer.
+# express-jwt-xsrf [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+Simple wrapper around postgresql transactions using pg.
+
+## Install
+Include with "npm install" or "yarn add":
+
+```bash
+yarn add express-jwt-xsrf --dev
+```
+
+## Usage
+
+...
+
+```javascript
+...
+```
+
+## License
+
+MIT © [Kirk Bulis](http://github.com/kbulis)
+
+[npm-image]: https://badge.fury.io/js/express-jwt-xsrf.svg
+[npm-url]: https://npmjs.org/package/express-jwt-xsrf
+[travis-image]: https://travis-ci.org/kbulis/express-jwt-xsrf.svg?branch=master
+[travis-url]: https://travis-ci.org/kbulis/express-jwt-xsrf
+[daviddm-image]: https://david-dm.org/kbulis/express-jwt-xsrf.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/kbulis/express-jwt-xsrf
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import * as encrypt from 'crypto';
+import * as cookies from 'cookie-parser';
+import * as parsing from 'body-parser';
+import * as jwtoken from 'jsonwebtoken';
+import * as express from 'express';
+
+/**
+ * axsrfWithCors implementation.
+ * 
+ * ...
+ * 
+ * @author Kirk Bulis
+ */
+function axsrfWithCors(options: {
+  hashingSecretKey: string,
+  hashingAlgorithm: string,
+  allowOrigin?: string,
+  jsonLimit?: number,
+  identify: (res: express.Response, member?: any) => void,
+}): express.RequestHandler[] {
+  return [
+    parsing.json({
+      limit: options.jsonLimit || 64 * 1024,
+    }),
+    cookies('', {
+      decode: (value) => {
+        return value;
+      }
+    }),
+    (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.header('Access-Control-Allow-Headers', req.header('Access-Control-Request-Headers'));
+      res.header('Access-Control-Allow-Origin', options.allowOrigin ? options.allowOrigin === '*' ? req.header('Origin') : options.allowOrigin : '*');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Vary', 'Origin');
+
+      next();
+    },
+    (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      try {
+        let encoded = req.headers['authorization'] as string || '';
+        
+        if (encoded.length > 6 && encoded.substr(0, 6).toLowerCase() === 'bearer') {
+          encoded = encoded.substr(7).trim();
+        }
+    
+        if (encoded.length === 0) {
+          encoded = req.query['token'] as string || '';
+        }
+    
+        if (encoded === 'null') {
+          encoded = '';
+        }
+        
+        if (encoded.length !== 0) {
+          try {
+            let extracted: { axsrf: string, iat: number, exp: number } = {
+              ...(jwtoken.verify(encoded, options.hashingSecretKey) as any),
+            };
+    
+            if (extracted.exp && extracted.exp > (+new Date / 1000)) {
+              if (req.cookies['axsrf'] && extracted.axsrf === encrypt.createHmac(options.hashingAlgorithm, options.hashingSecretKey).update(req.cookies['axsrf']).digest('base64')) {
+                options.identify(res, extracted);
+
+                next();
+
+                return;
+              }
+            }
+          }
+          catch (eX) {
+            console.log(eX);
+          }
+        }
+      }
+      catch (eX) {
+      }
+    
+      options.identify(res);
+    
+      next();
+    },
+  ];
+}
+
+export {
+  axsrfWithCors,
+};
