71f3130715efd5a9873b54049248109c65ddc20a
kbulis <kirk.d.bulis@gmail.com>
2018-03-08 15:10:37 -0800

    Fixed dependencies and jwt generation

--- src/index.ts
+++ src/index.ts
@@ -38,25 +38,25 @@ function axsrfWithCors(options: {
     (req: express.Request, res: express.Response, next: express.NextFunction) => {
       try {
         let encoded = req.headers['authorization'] as string || '';
-        
+
         if (encoded.length > 6 && encoded.substr(0, 6).toLowerCase() === 'bearer') {
           encoded = encoded.substr(7).trim();
         }
-    
+
         if (encoded.length === 0) {
           encoded = req.query['token'] as string || '';
         }
-    
+
         if (encoded === 'null') {
           encoded = '';
         }
-        
+
         if (encoded.length !== 0) {
           try {
             let extracted: { axsrf: string, iat: number, exp: number } = {
               ...(jwtoken.verify(encoded, options.hashingSecretKey) as any),
             };
-    
+
             if (extracted.exp && extracted.exp > (+new Date / 1000)) {
               if (req.cookies['axsrf'] && extracted.axsrf === encrypt.createHmac(options.hashingAlgorithm, options.hashingSecretKey).update(req.cookies['axsrf']).digest('base64')) {
                 options.identify(res, extracted);
@@ -74,9 +74,30 @@ function axsrfWithCors(options: {
       }
       catch (eX) {
       }
-    
+
       options.identify(res);
-    
+
+      next();
+    },
+    (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      if (!res.locals.generateTokenWithAxsrf) {
+        res.locals.generateTokenWithAxsrf = (payload: any, expiresIn: string, onlySecure: boolean) => {
+          const axsrf = 'a' + (Math.floor(Math.random() * 900000000000000) + 100000000000000);
+
+          res.cookie('axsrf', axsrf, {
+            httpOnly: true,
+            secure: onlySecure,
+          });
+
+          return jwtoken.sign({
+            axsrf: encrypt.createHmac(options.hashingAlgorithm, options.hashingSecretKey).update(axsrf).digest('base64'),
+            ...payload
+          }, options.hashingSecretKey, {
+              expiresIn: expiresIn,
+          }) as string;
+        };
+      }
+
       next();
     },
   ];
